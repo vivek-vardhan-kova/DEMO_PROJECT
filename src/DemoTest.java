65c0aedef919b37a884306f140bd5422dc850bab
Vivek Vardhan Kova <vivek.vardhankova@blretv-c02z2due.fkipl.flipkart.com>
2024-01-23 15:56:31 +0530

    java files added

---
+++ src/DemoTest.java
@@ -0,0 +1,13 @@
+
+public class DemoTest {
+
+	public DemoTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("workspace");
+	}
+
+}
